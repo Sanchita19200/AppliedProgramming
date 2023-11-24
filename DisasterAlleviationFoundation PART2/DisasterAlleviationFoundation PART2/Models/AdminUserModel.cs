c0cb8040f6902441bf8fee46cdc057ba7d233a3c
Sanchita <scarlet.ff1920@gmail.com>
2023-11-24 18:34:52 +0200

    Total Amount Page Changes

--- DisasterAlleviationFoundation PART2/DisasterAlleviationFoundation PART2/Models/AdminUserModel.cs
+++ DisasterAlleviationFoundation PART2/DisasterAlleviationFoundation PART2/Models/AdminUserModel.cs
@@ -2,22 +2,23 @@
 
 namespace DisasterAlleviationFoundation.Models
 {
+    // Represents the data model for an administrative user.
     public class AdminUserModel
     {
+        // Primary key for the AdminUserModel.
         [Key]
         public int AID { get; set; }
-        [Required]
-        [Display(Name = "Admin username")]
+
+        // Admin username property with required validation and display name.
+        [Required(ErrorMessage = "Admin username is required."), Display(Name = "Admin username")]
         public string UserName { get; set; }
 
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email Address")]
+        // Email property with required validation, email address format, and display name.
+        [Required(ErrorMessage = "Email Address is required."), EmailAddress(ErrorMessage = "Invalid Email Address."), Display(Name = "Email Address")]
         public string Email { get; set; }
 
-        [Required]
-        [Display(Name = "Password")]
-        [DataType(DataType.Password)]
+        // Password property with required validation, display name, and data type set to password.
+        [Required(ErrorMessage = "Password is required."), Display(Name = "Password"), DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
--- DisasterAlleviationFoundation PART2/DisasterAlleviationFoundation PART2/Models/GoodsDonationModel.cs
+++ DisasterAlleviationFoundation PART2/DisasterAlleviationFoundation PART2/Models/GoodsDonationModel.cs
@@ -1,46 +1,53 @@
-﻿using Microsoft.AspNetCore.Cors;
-using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Cors; // CORS attribute for enabling Cross-Origin Resource Sharing
+using System;
+using System.ComponentModel.DataAnnotations; // Validation attributes
 
 namespace DisasterAlleviationFoundation.Models
 {
+    // Represents the data model for goods donation in the Disaster Alleviation Foundation.
     public class GoodsDonationModel
     {
+        // Primary key for the GoodsDonationModel.
         [Key]
         public int GID { get; set; }
 
-        [Required]
-        [Display(Name ="Number of Items")]
+        // Number of items being donated with required validation and display name.
+        [Required(ErrorMessage = "Number of items is required."), Display(Name = "Number of Items")]
         public int numberofItems { get; set; }
 
-        [Required]
-        [Display(Name ="Good's Name")]
+        // Name of the donated good with required validation and display name.
+        [Required(ErrorMessage = "Good name is required."), Display(Name = "Good's Name")]
         public string Goodname { get; set; }
-        [Required]
-        [Display(Name = "Description")]
-        public string Gooddescription { get;set;}
 
-        [Required]
-        [Display(Name ="Product Category")]
+        // Description of the donated good with required validation and display name.
+        [Required(ErrorMessage = "Good description is required."), Display(Name = "Description")]
+        public string Gooddescription { get; set; }
+
+        // Category of the donated good with required validation and display name.
+        [Required(ErrorMessage = "Product category is required."), Display(Name = "Product Category")]
         public string category { get; set; }
-       
-        [Required]
-        [Display(Name = "Anonymous Donation")]
+
+        // Flag indicating whether the donation is anonymous with required validation and display name.
+        [Required(ErrorMessage = "Anonymous donation field is required."), Display(Name = "Anonymous Donation")]
         public bool IsAnonymous { get; set; }
 
-        public string customCategory { get; set; } // Added property for custom category
+        // Property for a custom category, if needed.
+        public string customCategory { get; set; }
 
-        [Required]
-        [Display(Name ="Date")]
+        // Date of the donation with required validation and display name.
+        [Required(ErrorMessage = "Date is required."), Display(Name = "Date")]
         public DateTime Date { get; set; }
 
+        // Properties for displaying to the admin.
 
-
-        //This is for displaying to the admin//
+        // Name of the donator.
         public string DonatorsName { get; set; }
+
+        // Category of the donation.
         public string Category { get; set; }
 
-        [Required]
-        [Display(Name = "Enter The Price of the Good")]
-        public int GoodsPrice { get; set;}
+        // Price of the donated good with required validation and display name.
+        [Required(ErrorMessage = "Price of the good is required."), Display(Name = "Enter The Price of the Good")]
+        public int GoodsPrice { get; set; }
     }
 }
